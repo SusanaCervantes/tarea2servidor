7c8bb49b61969650b5b522b267fa080e57bc73f1
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2019-09-23 15:59:00 -0600

    actividad

---
+++ AdmProWS/src/java/Model/ActividadDto.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Pablo-VE
+ */
+
+@XmlRootElement (name = "ActividadDto")
+@XmlAccessorType (XmlAccessType.FIELD)
+    
+public class ActividadDto {
+    private Long id;
+    private String nombre;
+    private String encargado; 
+    private String fpinicio; 
+    private String fpfinal;
+    private String frinicio; 
+    private String frfinal; 
+    private String descripcion;
+    private String estado;
+    private Long orden;
+    private Long version;
+    //proyecto
+
+    public ActividadDto(Long id, String nombre, String encargado, String fpinicio, String fpfinal, String frinicio, String frfinal, String descripcion, String estado, Long orden, Long version) {
+        this.id = id;
+        this.nombre = nombre;
+        this.encargado = encargado;
+        this.fpinicio = fpinicio;
+        this.fpfinal = fpfinal;
+        this.frinicio = frinicio;
+        this.frfinal = frfinal;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.orden = orden;
+        this.version = version;
+        //proyecto
+    }
+
+    public ActividadDto() {
+    }
+    
+//    public ActividadDto(Actividad act) {
+//        this.id = act.getActId();
+//        this.nombre = act.getActNombre();
+//        this.encargado = act.getActEncargado();
+//        this.fpinicio = act.getActFpInicio();
+//        this.fpfinal = act.getActFpFinal();
+//        this.frinicio = act.getActFrInicio();
+//        this.frfinal = act.getActFrFinal();
+//        this.descripcion = act.getActDescripcion();
+//        this.estado = act.getActEstado();
+//        this.orden = act.getActOrden();
+//        this.version = act.getActVersion();
+//        //proyecto
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEncargado() {
+        return encargado;
+    }
+
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+
+    public String getFpinicio() {
+        return fpinicio;
+    }
+
+    public void setFpinicio(String fpinicio) {
+        this.fpinicio = fpinicio;
+    }
+
+    public String getFpfinal() {
+        return fpfinal;
+    }
+
+    public void setFpfinal(String fpfinal) {
+        this.fpfinal = fpfinal;
+    }
+
+    public String getFrinicio() {
+        return frinicio;
+    }
+
+    public void setFrinicio(String frinicio) {
+        this.frinicio = frinicio;
+    }
+
+    public String getFrfinal() {
+        return frfinal;
+    }
+
+    public void setFrfinal(String frfinal) {
+        this.frfinal = frfinal;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Long getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Long orden) {
+        this.orden = orden;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+    
+}
+    
+
---
+++ AdmProWS/src/java/Service/ActividadService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Pablo-VE
+ */
+@Stateless
+public class ActividadService {
+    private static final Logger LOG = Logger.getLogger(ActividadService.class.getName());
+    
+    @PersistenceContext(unitName = "AdmProWsPU")
+    private EntityManager em; 
+    
+    
+//     public List<ActividadDto> getAdministradores(String nombre, String apellidos) {
+//        try {
+//            Query qryAdministrador = em.createNamedQuery("Administrador.findByNombreApellidos", Administrador.class);
+//            qryAdministrador.setParameter("nombre", nombre );
+//            qryAdministrador.setParameter("apellidos", apellidos );
+//            List<Administrador> administradores = qryAdministrador.getResultList();
+//            List<AdministradorDto> administradoresDto = new ArrayList<>();
+//            
+//            for (Administrador administrador : administradores) {
+//                administradoresDto.add(new AdministradorDto(administrador));
+//            }
+//            return administradoresDto;
+//        } catch (NoResultException ex) {
+//            return null;//new Respuesta(Boolean.FALSE, CodigoRespuesta.ERROR_NOENCONTRADO, "No se encontro ningun administrador con esos datos", "No encontrado");
+//        } catch (Exception ex) {
+//            LOG.log(Level.SEVERE, "Ocurrio un error al consultar el administrador.", ex);
+//            return null;//new Respuesta(Boolean.FALSE, CodigoRespuesta.ERROR_NOENCONTRADO, "Ocurrio un error al buscar el admnistrador", ex.toString());
+//        }
+//    }
+    
+}
